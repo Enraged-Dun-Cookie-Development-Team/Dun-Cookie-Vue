eea943f08ea1fee4703a42695cc1911bb9eca7ee
lwt-414 <2957035701@qq.com>
2022-05-02 17:01:12 +0800

    ✨ nodejs监听ws更换成来自配置文件

--- nodejs/test.js
+++ nodejs/test.js
@@ -3,10 +3,16 @@ import {Worker} from "worker_threads";
 import http from "http";
 import urlib from "url";
 import CardList from "../src/common/sync/CardList";
+import ws from "ws"
+import fs from "fs"
 
-new Worker('../src/background/index.js', {
-  execArgv: ['--experimental-loader', './loader.mjs']
+const worker = new Worker('../src/background/index.js', {
+  execArgv: ['--experimental-loader', './loader.mjs'],
 });
+
+worker.onerror = (e) => {
+  console.log(e);
+};
 
 Object.keys(DunInfo);
 
@@ -26,8 +32,12 @@ Object.keys(DunInfo);
 //   }
 // }).listen(3000); // 绑定3000端口
 
+// 读取配置文件
+let rawdata = fs.readFileSync("config.json");
+let config = JSON.parse(rawdata);
+
 // 创建了一个客户端的socket,然后让这个客户端去连接服务器的socket
-let sock = new ws("ws://127.0.0.1:5683");
+let sock = new ws("ws://"+config.ws.host+":"+config.ws.port);
 sock.on("open", _ => {
   console.log("与服务端建立链接成功")
 	CardList.doAfterUpdate(data => {
@@ -43,7 +53,7 @@ sock.on("close", _ => {
 	console.log("检测到服务端关闭");
 });
 
-sock.on("message", function(data) {
+sock.on("message", data => {
 	// console.log(data);
 });
 
