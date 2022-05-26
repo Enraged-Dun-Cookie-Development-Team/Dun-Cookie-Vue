329e5a439ccae01e9892b3a1d4abaeda463af61a
lwt-414 <2957035701@qq.com>
2022-05-27 01:00:57 +0800

    ✨ 完成插件更新后弹出一次更新内容提醒

--- src/components/UpdateInfoNotice.vue
+++ src/components/UpdateInfoNotice.vue
@@ -1,15 +1,14 @@
 <template>
-  <el-dialog
-      :title="'小刻食堂 V'+updateInfo.v+' 翻新了什么？'"
-      :modal-append-to-body="false"
-      :visible.sync="showUpdateInfo"
-      @closed="closeDialog"
-      width="80%">
+  <el-dialog :title="'小刻食堂 V' + updateInfo.v + ' 翻新了什么？'" :modal-append-to-body="false" :visible.sync="showUpdateInfo"
+    width="80%">
     <div class="update-info-area" v-html="updateInfo.description"></div>
   </el-dialog>
 </template>
 
 <script>
+import PlatformHelper from "../common/platform/PlatformHelper";
+import { CURRENT_VERSION } from "../common/Constants";
+
 export default {
   name: "UpdateInfoNotice",
   props: ["updateInfo"],
@@ -23,8 +22,15 @@ export default {
   },
   methods: {
     init() {
-      setTimeout(() => {
-        this.showUpdateInfo = true;
+      setTimeout(async () => {
+        if (CURRENT_VERSION === this.updateInfo.v) {
+          let versionNotice = await PlatformHelper.Storage.getLocalStorage('version-notice');
+          if (!versionNotice) {
+            this.showUpdateInfo = true;
+            versionNotice = true;
+            PlatformHelper.Storage.saveLocalStorage('version-notice', versionNotice);
+          }
+        }
       }, 1000);
     }
   }
