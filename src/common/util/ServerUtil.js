78ceb4f31747e4968dfbf1f39a819316d6063a22
lwt-414 <2957035701@qq.com>
2022-08-16 12:58:08 +0800

    👔 添加获取服务端运营信息对应逻辑

--- src/common/util/ServerUtil.js
+++ src/common/util/ServerUtil.js
@@ -5,7 +5,7 @@ import HttpUtil from "./HttpUtil";
 import PlatformHelper from "../platform/PlatformHelper";
 import InsiderUtil from "./InsiderUtil";
 import Settings from "../Settings";
-import { CANTEEN_INTERFACE_LIST, CURRENT_VERSION } from "../Constants";
+import { CANTEEN_INTERFACE_LIST, CANTEEN_SERVER_LIST, CURRENT_VERSION } from "../Constants";
 import NotificationUtil from "./NotificationUtil";
 import TimeUtil from "./TimeUtil";
 import PromiseUtil from "./PromiseUtil";
@@ -69,6 +69,141 @@ export default class ServerUtil {
                     }
                 })
                 PlatformHelper.Storage.saveLocalStorage("announcement-notice-status", announcementNoticeStatus)
+            }
+        }
+        return data;
+    }
+
+    /**
+     * 获取公告信息
+     */
+    static async getAnnouncementInfo(shouldNotice) {
+        await new Promise(resolve => Settings.doAfterInit(() => resolve()));
+        let data;
+        try {
+            data = await PromiseUtil.any(CANTEEN_SERVER_LIST.map(api => HttpUtil.GET_Json(api + "canteen/operate/announcement/list", false)), res => !!res);
+            console.log(data)
+            data = data.data
+        } catch (e) {
+            console.log(e);
+        }
+        if (!data) {
+            const fallbackUrl = PlatformHelper.Extension.getURL("Dun-Cookies-Info.json");
+            data = await HttpUtil.GET_Json(fallbackUrl);
+            data = data.list
+        }
+        if (!data) {
+            return data;
+        }
+        if (shouldNotice) {
+            if (Settings.feature.announcementNotice) {
+                let filterList = data.filter(
+                    (x) =>
+                        new Date(x.starTime) <= TimeUtil.changeToCCT(new Date()) &&
+                        new Date(x.overTime) >= TimeUtil.changeToCCT(new Date())
+                );
+
+                let today = TimeUtil.format(new Date(), 'yyyy-MM-dd');
+                let announcementNoticeStatus = await PlatformHelper.Storage.getLocalStorage("announcement-notice-status") || {};
+
+                // 判断当天是否推送过
+                filterList.map(x => {
+                    if (x.notice) {
+                        if (!announcementNoticeStatus[today]) {
+                            announcementNoticeStatus = {};
+                            announcementNoticeStatus[today] = {};
+                        }
+                        if (!announcementNoticeStatus[today][today +"-"+ x.notice]) {
+                            announcementNoticeStatus[today][today + "-" + x.notice] = true;
+                            let imgReg = /<img.*?src='(.*?)'/;
+                            let imgUrl = x.html.match(imgReg)[1];
+                            let removeTagReg = /<\/?.+?\/?>/g;
+                            let divReg = /<\/div>/g;
+
+                            let content = x.html.replace(/\s+/g, '');
+                            content = content.replace(divReg, '\n');
+                            content = content.replace(removeTagReg, '');
+
+                            imgUrl = imgUrl == "/assets/image/" + Settings.logo ? "/assets/image/announcement.png" : imgUrl;
+
+                            NotificationUtil.SendNotice("博士，重要公告，记得开列表看噢！", content, imgUrl, "announcement" + new Date().getTime());
+                        }
+                    }
+                })
+                PlatformHelper.Storage.saveLocalStorage("announcement-notice-status", announcementNoticeStatus)
+            }
+        }
+        return data;
+    }
+
+    /** 
+     * 获取视频信息
+     */
+    static async getVideoInfo() {
+        await new Promise(resolve => Settings.doAfterInit(() => resolve()));
+        let data;
+        try {
+            data = await PromiseUtil.any(CANTEEN_SERVER_LIST.map(api => HttpUtil.GET_Json(api + "canteen/operate/video/list", false)), res => !!res);
+            data = data.data
+        } catch (e) {
+            console.log(e);
+        }
+        if (!data) {
+            const fallbackUrl = PlatformHelper.Extension.getURL("Dun-Cookies-Info.json");
+            data = await HttpUtil.GET_Json(fallbackUrl);
+            data = data.btnList
+        }
+        return data;
+    }
+
+    /** 
+     * 获取公告资源信息
+     */
+    static async getResourceInfo() {
+        await new Promise(resolve => Settings.doAfterInit(() => resolve()));
+        let data;
+        try {
+            data = await PromiseUtil.any(CANTEEN_SERVER_LIST.map(api => HttpUtil.GET_Json(api + "canteen/operate/resource/get", false)), res => !!res);
+            data = data.data
+        } catch (e) {
+            console.log(e);
+        }
+        if (!data) {
+            const fallbackUrl = PlatformHelper.Extension.getURL("Dun-Cookies-Info.json");
+            data = await HttpUtil.GET_Json(fallbackUrl);
+            data = data.dayInfo
+        }
+        return data;
+    }
+
+    /**
+     * @param currentVersion {boolean} 是否获取当前版本信息
+     * @param shouldNotice {boolean}
+     */
+    static async getVersionInfo(currentVersion, shouldNotice) {
+        await new Promise(resolve => Settings.doAfterInit(() => resolve()));
+        let data;
+        try {
+            if (currentVersion) {
+                data = await PromiseUtil.any(CANTEEN_SERVER_LIST.map(api => HttpUtil.GET_Json(api + "canteen/operate/version/plugin?version=" + CURRENT_VERSION, false)), res => !!res);
+            } else {
+                data = await PromiseUtil.any(CANTEEN_SERVER_LIST.map(api => HttpUtil.GET_Json(api + "canteen/operate/version/plugin", false)), res => !!res);
+            }
+            data = data.data
+        } catch (e) {
+            console.log(e);
+        }
+        if (!data) {
+            const fallbackUrl = PlatformHelper.Extension.getURL("Dun-Cookies-Info.json");
+            data = await HttpUtil.GET_Json(fallbackUrl);
+            data = data.upgrade
+        }
+        if (!data) {
+            return data;
+        }
+        if (shouldNotice) {
+            if (Settings.JudgmentVersion(data.upgrade.v, CURRENT_VERSION) && Settings.dun.enableNotice) {
+                NotificationUtil.SendNotice("小刻食堂翻新啦！！", "快来使用新的小刻食堂噢！一定有很多好玩的新功能啦！！", null, "update");
             }
         }
         return data;
