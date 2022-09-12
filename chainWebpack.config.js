040f0e9e534a53691b4a75b7d8d54d429985976c
Yun Shan <yunshan21st@qq.com>
2022-09-12 17:48:54 +0800

    fix dependencies

--- chainWebpack.config.js
+++ chainWebpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const ExtensionReloader = require('webpack-extension-reloader');
+const ExtensionReloader = require('webpack-ext-reloader');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const isDevMode = process.env.NODE_ENV === 'development';
