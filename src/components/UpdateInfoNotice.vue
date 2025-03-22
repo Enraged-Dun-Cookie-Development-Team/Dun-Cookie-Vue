7fc564c214bc1596033217ed433ed68d23a6fddb
洛梧藤 <2957035701@qq.com>
2025-03-23 02:16:33 +0800

    🐛 修复版本列表刷新生效，修复css样式

--- src/components/UpdateInfoNotice.vue
+++ src/components/UpdateInfoNotice.vue
@@ -1,21 +1,23 @@
 <template>
   <el-dialog
     v-if="updateInfo.list.length > 0"
-    :title="'小刻食堂 V' + updateInfo.list[0].version + ' 翻新了什么？'"
+    :title="'小刻食堂翻新了什么？'"
     :modal-append-to-body="false"
     :visible.sync="showUpdateInfo"
     class="update-info-dialog"
   >
     <div ref="observeContainer" class="update-info-area">
-      <div class="heading">{{ '新版本V' + updateInfo.list[0].version }}</div>
-      <div class="currDescription">{{ updateInfo.list[0].description }}</div>
+      <div>
+        <div class="heading">{{ '新版本V' + updateInfo.list[0].version }}</div>
+        <div class="current-description">{{ updateInfo.list[0].description }}</div>
 
-      <div class="heading">{{ '历史版本' }}</div>
-      <div v-for="(info, index) in updateInfo.list.slice(1)" :key="index">
-        <div class="historyVersionH">{{ 'V' + info.version }}</div>
-        <div class="hisDescription">{{ info.description }}</div>
+        <div class="heading">{{ '历史版本' }}</div>
+        <div v-for="(info, index) in updateInfo.list.slice(1)" :key="index">
+          <div class="history-version">{{ 'V' + info.version }}</div>
+          <div>{{ info.description }}</div>
+        </div>
+        <div ref="bottomChecker" class="bottom-check"></div>
       </div>
-      <div ref="bottomChecker" class="bottomChecker"></div>
     </div>
   </el-dialog>
 </template>
@@ -57,6 +59,7 @@ export default {
     },
     //懒加载请求数据
     async loadData() {
+      this.loading = true;
       const data = await ServerUtil.getVersionHistory('plugin', this.nextPageId);
       if (data) {
         //将懒加载数据加进updateInfoList
@@ -67,6 +70,7 @@ export default {
         }
         this.nextPageId = data.next_id;
       }
+      this.loading = false;
     },
 
     // 设置 IntersectionObserver 监听滚动到达底部加载下一页数据
@@ -91,57 +95,49 @@ export default {
 </script>
 
 <style lang="less" scoped>
-.update-info-area {
-  overflow-y: auto;
-  padding: 15px;
-  font-size: 14px;
-  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-  white-space: pre-wrap;
-  flex: 1;
-}
-.update-info-dialog ::v-deep .el-dialog {
-  display: flex;
-  margin: 10vh auto auto;
-  width: '80%';
-  flex-direction: column;
-}
+.update-info-dialog {
+  :deep(.el-dialog) {
+    position: relative;
+    top: 0;
+    z-index: 10;
+    display: flex;
+    padding: 15px;
+    width: 80%;
+    border-bottom: 1px solid #ddd;
+    flex-direction: column;
+    flex: 1;
+  }
 
-.update-info-dialog ::v-deep .el-dialog__header {
-  position: relative;
-  top: 0;
-  z-index: 10;
-  padding: 15px;
-  border-bottom: 1px solid #ddd;
-  background: white;
-  flex: 0 0 auto;
-}
+  .update-info-area {
+    overflow-y: scroll;
+    padding: 0 15px;
+    height: 50vh;
+    font-size: 14px;
+    white-space: pre-wrap;
 
-.update-info-dialog ::v-deep .el-dialog__body {
-  overflow-y: auto;
-  padding: 15px;
-  max-height: calc(80vh - 60px); /* 计算除去 header 和 footer 的高度 */
-  flex: 1;
-}
-.bottomCheck {
-  margin-top: -50px;
-  width: 100%;
-  height: 50px;
-  pointer-events: none;
-}
+    .heading {
+      margin-bottom: 8px;
+      font-size: 18px;
+      font-weight: 500;
+      color: #007bff;
+    }
 
-.heading {
-  margin-bottom: 8px;
-  font-size: 18px;
-  font-weight: 500;
-  color: #007bff;
-}
+    .current-description {
+      margin-bottom: 20px;
+    }
 
-.historyVersionH {
-  margin: 10px 0;
-  font-size: 14px;
-  font-weight: 500;
-}
-.currDescription {
-  margin-bottom: 20px;
+    .history-version {
+      margin: 10px 0;
+      font-size: 14px;
+      font-weight: 500;
+    }
+
+    .bottom-check {
+      margin-top: -50px;
+      width: 100%;
+      height: 50px;
+      pointer-events: none;
+    }
+  }
 }
 </style>
